af9a09051aea77b4e2cac8a44374b53322d097e1
JGo <joe@siluri.de>
2017-09-21 13:28:15 +0200

    Rest Anbindung

--- app.js
+++ app.js
@@ -16,11 +16,13 @@
 
 'use strict';
 
-var express = require('express'); // app server
-var bodyParser = require('body-parser'); // parser for post requests
-var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+const express = require('express'); // app server
+const bodyParser = require('body-parser'); // parser for post requests
+const Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+const http = require('http');
+const request = require('xhr-request')
 
-var app = express();
+const app = express();
 
 // Bootstrap application settings
 app.use(express.static('./public')); // load UI from public folder
@@ -28,69 +30,128 @@ app.use(bodyParser.json());
 
 // Create the service wrapper
 var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
-  // url: 'https://gateway.watsonplatform.net/conversation/api',
-  version_date: Conversation.VERSION_DATE_2017_04_21
+    // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+    // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+    // username: '<username>',
+    // password: '<password>',
+    // url: 'https://gateway.watsonplatform.net/conversation/api',
+    version_date: Conversation.VERSION_DATE_2017_04_21
 });
 
 // Endpoint to be call from the client side
-app.post('/api/message', function(req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
-  var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
-  };
-
-  // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      return res.status(err.code || 500).json(err);
+app.post('/api/message', function (req, res) {
+    var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+    if (!workspace || workspace === '<workspace-id>') {
+        return res.json({
+            'output': {
+                'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the '
+                + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>'
+                + 'Once a workspace has been defined the intents may be imported from '
+                + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+            }
+        });
     }
-    return res.json(updateMessage(payload, data));
-  });
+    var payload = {
+        workspace_id: workspace,
+        context: req.body.context || {},
+        input: req.body.input || {}
+    };
+
+    // Send the input to the conversation service
+    conversation.message(payload, function (err, data) {
+        if ( err ) {
+            return res.status( err.code || 500 ).json( err );
+        }
+        updateMessage( res, payload, data );
+    });
 });
 
 /**
  * Updates the response text using the intent confidence
+ * @param  {Object} res The node.js http response object
  * @param  {Object} input The request to the Conversation service
  * @param  {Object} response The response from the Conversation service
  * @return {Object}          The response with the updated message
  */
-function updateMessage(input, response) {
-  var responseText = null;
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
+function updateMessage(res, input, response) {
+    var acl = false;
+    if (!response.output) {
+        response.output = {};
     } else {
-      responseText = 'I did not understand your intent';
+        if (response.intents && response.intents[0]) {
+            var intent = response.intents[0];
+            if(intent.intent === 'countries') { //TODO: evtl. intent.confidence
+                getEUStaaten(res, response);
+            } else if(intent.intent === 'capital') {
+                getCapitalStaaten(res, response, response.entities[0]);
+            } else {
+                return res.json( response );
+            }
+        } else {
+            return res.json( response );
+        }
     }
-  }
-  response.output.text = responseText;
-  return response;
-}
+};
 
+/**
+ * @name getCapitalStaaten - collect Capital for one Country
+ * @param res
+ * @param responseText
+ * @param repsonse
+ */
+function getCapitalStaaten(res, response, entity){
+    log(entity);
+    var opt = {
+        host: 'https://restcountries.eu',
+        path: '/rest/v2/name/',
+        get: 'Poland' //TODO
+    };
+    request(opt.host + opt.path + opt.get, {
+        json: true
+    }, function (err, country) {
+        if (err) {
+            console.error(err);
+        } else {
+            var respText = response.output.text + '';
+            response.output.text = respText.replace('{capital}', country[0].capital);
+            return res.json( response );
+        }
+    });
+};
+/**
+ * @name getEUStaaten - collect all EU Countries
+ * @param res
+ * @param responseText
+ * @param response
+ */
+function getEUStaaten(res, response) {
+    var opt = {
+        host: 'https://restcountries.eu',
+        path: '/rest/v2/regionalbloc/',
+        get: 'EU'
+    };
+    request(opt.host + opt.path + opt.get, {
+        json: true
+    }, function (err, countries) {
+        if (err) {
+            console.error(err);
+        } else {
+            let responseText = '';
+            for(var i in countries){
+                var country  = countries[i];
+                if(country.name !== null){
+                    responseText += (country.name + ' ');
+                }
+            }
+            var respText = response.output.text + '';
+            response.output.text = respText.replace('{countries}', responseText);
+            log('before res.json', responseText);
+            return res.json( response );
+        }
+    });
+};
+
+function log() {
+    console.log('####: ', arguments);
+};
 module.exports = app;
